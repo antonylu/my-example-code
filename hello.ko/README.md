0beccb71d0226ba4a408c84a9cbd60b9c41ffab0
Antony Lu <antony_lu@compal.com>
2017-12-29 15:13:28 +0800

    Add hello.ko kernel module example code

---
+++ hello.ko/README.md
@@ -0,0 +1,9 @@
+# kernel module example Code
+
+Usage:
+* make
+* insmod hello.ko
+* rmmod hello.ko
+* tail /proc/kmsg
+* udevadm monitor
+
---
+++ hello.ko/hello.c
@@ -0,0 +1,17 @@
+#include <linux/init.h>
+#include <linux/module.h>
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int hello_init(void)
+{
+	printk(KERN_INFO "Hello kernel\n");
+	return 0;
+}
+
+static void hello_exit(void)
+{
+	printk(KERN_INFO "Goodbye\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
