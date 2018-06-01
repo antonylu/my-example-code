4c7e9f1e9b2a09d987704bf44b0edde447e4333e
Antony Lu <w3back@gmail.com>
2018-06-01 22:00:19 +0800

    Add Transparent background color to PNG

--- line-sticker/sg.py
+++ line-sticker/sg.py
@@ -8,14 +8,21 @@ The sticker size is:
 * 370 x 320, with 10 px blank in each side
 * 350 x 300, generate canvas
 
+Requirements:
+* fit the size 350x300
+* font size as big as possible, minimum size is 70
+* all chars must fit in the canvas
+* align in center
+* breaks to two lines automatically
+
 """
 from PIL import Image,ImageDraw,ImageFont
 
-#font = ImageFont.truetype('simsun.ttc',24)
-font = ImageFont.truetype('setofont.ttf',24)
-img = Image.new('RGB',(370,320),(255,255,255))
-draw = ImageDraw.Draw(img)
-draw.text( (0,50), u'你好,世界!',(0,0,0),font=font)
-#draw.text((0,60),str('你好','utf-8'),(0,0,0),font=font) 
-img.save("test.png")
-exit(0)
+
+if __name__ == '__main__':
+    #font = ImageFont.truetype('simsun.ttc',24)
+    font = ImageFont.truetype('setofont.ttf',70)
+    img = Image.new('RGBA',(370,320),(255,255,255,0))
+    draw = ImageDraw.Draw(img)
+    draw.text( (10,50), '你好,世界!',(0,0,0),font=font)
+    img.save("test.png")
